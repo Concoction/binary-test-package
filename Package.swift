906a3bba873f56f408e422d206003c1b0f259b09
John Holdsworth <>
2024-02-22 21:33:24 +0100

    Update frameworks

--- Package.swift
+++ Package.swift
@@ -25,17 +25,17 @@ let package = Package(
     .binaryTarget(
         name: "ModuleA",
         url: repo + "ModuleA.xcframework.zip",
-        checksum: "c8f8159682dcf824ec3259288521f212f46ed59269584862ff9d4064fc08b1e2"
+        checksum: "36e003bb5c4b6160d5da6a595f37685b06acfb7bdfbbf92b156dda24caf1090e"
     ),
     .binaryTarget(
         name: "ModuleB",
         url: repo + "ModuleB.xcframework.zip",
-        checksum: "ef77d743c6dd2a6cbedb89a1af2ec4565a7b56c39a440a7d7652a8822a158b59"
+        checksum: "d5774e0883f6f7037ee7018823363d25da84923e409647c752f88787e0e7e255"
     ),
     .binaryTarget(
         name: "ModuleC",
         url: repo + "ModuleC.xcframework.zip",
-        checksum: "824ccfcbae72f1828a019a18859b6166b4669d0949f1902eb20b552834e1d527"
+        checksum: "c0f4574c4d2386e4144201d7372d6da74d55de0dd63b4baaab16e5abd512cbe6"
     ),
   ]
 )

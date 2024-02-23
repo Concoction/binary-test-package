bf40b3ff5e9dd394260bf704ca169d3934c429d9
John Holdsworth <>
2024-02-23 14:57:18 +0100

    Update frameworks

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@ import CompilerPluginSupport
 import PackageDescription
 import Foundation
 
-let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.9/"
+let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.10/"
 let modules = ["ModuleA", "ModuleB", "ModuleC"]
 
 let package = Package(
@@ -25,17 +25,17 @@ let package = Package(
     .binaryTarget(
         name: "ModuleA",
         url: repo + "ModuleA.xcframework.zip",
-        checksum: "b3b52338afdb38ef40b76763573b4d6241494d603972ae7f4c090bee619aa2eb"
+        checksum: "fec061643f7c79411decf5eb4240eaacc8b8d1933688ee9ee532105a878c0937"
     ),
     .binaryTarget(
         name: "ModuleB",
         url: repo + "ModuleB.xcframework.zip",
-        checksum: "3742a4545bdeaf9ae54d38e65a078612ab60a80eb7f6562ca3ad40ad66ff10ce"
+        checksum: "f30ce7e862570e3f9fa21fd10d93092f49df5415116db487a640c865b0295c70"
     ),
     .binaryTarget(
         name: "ModuleC",
         url: repo + "ModuleC.xcframework.zip",
-        checksum: "4ce940a19e7b575682b678320d338401afe3e7354a26031ab95473b02dfccec6"
+        checksum: "f152bf9399c008d70d94844dd6e9c3f0aa8250fadd3dc8bfe57e4dd20de914fc"
     ),
   ]
 )

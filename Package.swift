93b05bf78c62c3dca62de41784fd4ad22eb8f847
John Holdsworth <>
2024-02-25 16:01:23 +0100

    Update 1.0.9017

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 import CompilerPluginSupport
 import PackageDescription
 
-let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.9016/"
+let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.9017/"
 let modules = ["ModuleA", "ModuleB", "ModuleC"]
 
 let package = Package(
@@ -17,5 +17,20 @@ let package = Package(
 
   targets: [
     .target(name: "test-package_Binder"),
+    .binaryTarget(
+        name: "ModuleA",
+        url: repo + "ModuleA.xcframework.zip",
+        checksum: "a5a4fe43c8f23ddbe302576bf07fd0d617e370c5e54a2bce9167599293146f33"
+    ),
+    .binaryTarget(
+        name: "ModuleB",
+        url: repo + "ModuleB.xcframework.zip",
+        checksum: "323edbc36369037f9822c14818d5889e61b07506f3699d8a5229f5a5fe8ce0ec"
+    ),
+    .binaryTarget(
+        name: "ModuleC",
+        url: repo + "ModuleC.xcframework.zip",
+        checksum: "612cba2c1549e8b3ed4f21c2e855c60182183c055bbb216c1a1eb5142e2f6562"
+    ),
   ]
 )

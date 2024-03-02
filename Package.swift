4b70aae540dc5eab0b57cb927a92008a2705e934
John Holdsworth <>
2024-03-02 12:17:34 +0100

    Update 1.0.9028

--- Package.swift
+++ Package.swift
@@ -3,34 +3,39 @@
 import CompilerPluginSupport
 import PackageDescription
 
-let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.9027/"
-let modules = ["ModuleA", "ModuleB", "ModuleC"]
+let repo = "https://github.com/Concoction/binary-test-package/raw/1.0.9028/"
+let frameworks = [/*"ModuleC",*/ /*"test-package",*/ "ModuleA", "ModuleB"]
 
 let package = Package(
     name: "test-package",
 
 
-    products: modules.map {
+    products: ["ModuleA", "ModuleB"].map {
       .library(name: $0, type: .static, targets: [$0, "test-package_Binder"]
     ) },
 
 
     targets: [
-        .target(name: "test-package_Binder", dependencies: modules.map { .target(name: $0) }),
+        .target(name: "test-package_Binder", dependencies: frameworks.map { .target(name: $0) }),
+        .binaryTarget(
+            name: "ModuleC",
+            url: repo + "ModuleC.xcframework.zip",
+            checksum: "f8574016cd4ba48ea324fc7e7b5c2b448da4171c5c1c6844a5a7a55d854a02d9"
+        ),
+        .binaryTarget(
+            name: "test-package",
+            url: repo + "test-package.xcframework.zip",
+            checksum: "d398a26efb50f25f7864cf45b650e954cc760e7609d4d616ab56c82bb29fef3a"
+        ),
         .binaryTarget(
             name: "ModuleA",
             url: repo + "ModuleA.xcframework.zip",
-            checksum: "e57ef51282b9b6c6808cdd718f8159fc1946ed7613fbe9d3be109fcc4e764745"
+            checksum: "9653814e6bc4ca7042f030e9e98774b1de00016a258049aa81466158a3dea654"
         ),
         .binaryTarget(
             name: "ModuleB",
             url: repo + "ModuleB.xcframework.zip",
-            checksum: "5a964cb1792c5b190bd28e9e9a1fe205039afd0651250c0d16c26be7f01f0102"
-        ),
-        .binaryTarget(
-            name: "ModuleC",
-            url: repo + "ModuleC.xcframework.zip",
-            checksum: "ef1c6882fdc839d98b453623a02850ae14ebbed5972c830866a8cd01f196a4db"
+            checksum: "c3c1bb28e0221b8f24f99398a89d8907ddfee6286a8fbb69bb4707faf108469f"
         ),
     ]
 )
